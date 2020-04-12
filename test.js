2182d5299437f008631b33c2893977200577a94c
Nguyễn Hoàng Nam <congviec18120062@gmail.com>
2020-04-12 22:00:26 +0700

    🔥 Make save function

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 const mysqler = require("./index");
 
+mysqler.connect("mysql://root:root@localhost/test", (err) => {
+  if (err) throw err;
+
+  console.log("Connect success !!!");
+});
+
 const Schema = mysqler.Schema;
 const userSchema = new Schema({
   id: "int not null auto_increment primary key",
@@ -8,4 +14,16 @@ const userSchema = new Schema({
   password: "varchar(60) not null",
 });
 
-console.log(userSchema.columnDefinition());
+const User = mysqler.model("users", userSchema);
+
+const newUser = new User({
+  name: "abc",
+  email: "def",
+  password: "abcdef",
+});
+
+newUser.save((err, results) => {
+  if (err) throw err;
+
+  console.log(results);
+});
