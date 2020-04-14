6c22eb745a833164f1908c095d1703e09517ad1c
Nguyễn Hoàng Nam <congviec18120062@gmail.com>
2020-04-14 09:10:51 +0700

    📚 Count function _ Add countDocument function _ Add countDocument test

---
+++ test/countTest.js
@@ -0,0 +1,12 @@
+const User = require("./src/user");
+
+describe("Counting documents", () => {
+  it("counts all users", (done) => {
+    User.countDocuments({}, (err, results) => {
+      if (err) throw err;
+
+      console.log(results);
+      done();
+    });
+  });
+});
